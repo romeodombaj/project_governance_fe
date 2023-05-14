f9d7dc071ba317ccfdfb120efeb9f999d1d77307
romeodombaj <romeodombaj@gmail.com>
2023-05-14 16:01:50 +0200

    searching trough projects added

--- src/components/1_Project managment/ProjectProject.js
+++ src/components/1_Project managment/ProjectProject.js
@@ -1,17 +1,41 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import styles from "./ProjectProject.module.css";
 import useGetData from "../hooks/use-get-data";
 
 const ProjectProject = () => {
-
+  const [isCreatingNew, setIsCreatingNew] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
   let projectList = useGetData("project_managment/projects");
 
+  const onSearchHandler = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
-    <div className={styles.wrapper}>
-      {projectList.map((project) => {
-        return <div key={project._id}>{project.name}</div>;
-      })}
-    </div>
+    <Fragment>
+      {}
+      <div className={styles.wrapper}>
+        <div className={styles[`project-actions-wrapper`]}>
+          <input
+            className={styles.search}
+            onChange={onSearchHandler}
+            value={searchValue}
+          ></input>
+          <div className={styles[`add-button`]}>Add project</div>
+        </div>
+        <div className={styles[`project-list`]}>
+          {projectList.map((project) => {
+            if (project.name.includes(searchValue)) {
+              return (
+                <div className={styles[`project-wrapper`]} key={project._id}>
+                  {project.name}
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
